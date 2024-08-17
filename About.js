7e100be77bde3d033e80416d6b050ab87beb131d
KNavyatha2003 <kongithalasaritha@gmail.com>
2024-08-17 21:33:07 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,19 @@
+// src/About.js
+import React from 'react';
+import './About.css'
+const About = () => {
+  return (
+    <div>
+      <center>
+      <h3><b>I'm Kongithala Navyatha</b></h3>
+      <p>Student at Dhanekula Institute of engineering and technology</p>
+      <p>in the stream of IT</p>
+      <div className='img'>
+      <img src='g.jpg'></img>
+      </div>
+      </center>
+    </div>
+  );
+};
+
+export default About;
